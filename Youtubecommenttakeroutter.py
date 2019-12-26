d2c3d8117ed88b44873c6cc61e7aa31b0868ebc9
adidtdh <54081885+adidtdh@users.noreply.github.com>
2019-12-26 17:18:27 -0500

    Optimized code

--- Youtubecommenttakeroutter.py
+++ Youtubecommenttakeroutter.py
@@ -7,13 +7,17 @@ class GetComments:
     def __new__(cls, url, number, prefix):
         driver = webdriver.Chrome()
         driver.get(url)
-        comments = []
         html = driver.find_element_by_tag_name('html')
         sleep(3)
 
-        while len(comments) < number:
-            comments = [elem.text for elem in driver.find_elements_by_xpath('//*[@id="content-text"]')]
-            sleep(.5)
+        while len(driver.find_elements_by_xpath('//*[@id="content-text"]')) < number:
             html.send_keys(Keys.PAGE_DOWN)
+            print(len(driver.find_elements_by_xpath('//*[@id="content-text"]')))
+            sleep(.1)
+
+        comments = [elem.text for elem in driver.find_elements_by_xpath('//*[@id="content-text"]')]
 
         return ' '.join([str(elem) for elem in [f"\n{prefix}" + x for x in comments]])
+
+
+print(GetComments("https://www.youtube.com/watch?v=DU2AFIOnw_c", 300, "Comment:"))
